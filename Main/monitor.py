9e2d320a39cda8f40be1fc1033f9cb245352d2f9
Matias Egea <matiasegea@gmail.com>
2022-07-29 16:13:11 -0300

    more tests and minor fixes

--- Main/monitor.py
+++ Main/monitor.py
@@ -42,10 +42,12 @@ class Monitor():
 	def check_status(self, rig_name, rig_id, errors):
 		status = self.api.get_my_rig_stats(rig_id)
 		self.logger.info(status)
-		status = status["algorithms"]["DAGGERHASHIMOTO"]
-		if (not status["isActive"]):
-			self.error('[{}] host is disabled.'.format(rig_name), errors)
+		status = status["algorithms"]
+		if ("DAGGERHASHIMOTO" not in status or not status["DAGGERHASHIMOTO"]["isActive"]):
+			self.error('[{}] host is down.'.format(rig_name), errors)
 			return False
+		status = status["DAGGERHASHIMOTO"]
+
 		accepted=status["speedAccepted"]
 		rejected=status["speedRejected"]
 		if (accepted==0):
@@ -69,32 +71,45 @@ class Monitor():
 			for dev_id, values in self.devices.items():
 				if (dev_id in name):
 					self.logger.debug('profile: {} for {}'.format(dev_id, name))
-					self.check(rig_name, device, values.max_power, values.max_tem, values.min_hr, values.min_fan_speed, errors)
+					self.check(rig_name, device, values.max_power, values.max_core_temp, values.max_vram_temp, values.min_hr, values.min_fan_speed, errors)
 					found = True
 					break
 			if (not found):
 				self.error('Script error [{}.{}] device not recognized'.format(rig_name, name), errors, True)
 		return True
 
-	def check(self, rig_name, device, max_power, max_tem, min_hr, min_fan_speed, errors):
+	def check(self, rig_name, device, max_power, max_core_temp, max_vram_temp, min_hr, min_fan_speed, errors):
 		name= device["name"]
 		device_id= device["id"]
-		power = device["powerUsage"]
-		temp = device["temperature"]%65536
-		hr = float(device["speeds"][0]["speed"])
-		fan_speed = device["revolutionsPerMinutePercentage"]
 		status = device["status"]["enumName"]
 		if(status != "MINING"):
 			self.error('[{}.{}.{}] current status is {}.'.format(rig_name, name, device_id, status), errors)
 			return False
+		power = device["powerUsage"]
+		hr = float(device["speeds"][0]["speed"])
+		fan_speed = device["revolutionsPerMinutePercentage"]
+		temp_encoded = device["temperature"]
+		core_temp = temp_encoded % 65536
+		hot_spot_temp = temp_encoded / 65536
+		nhqm = device["nhqm"].split(";")
+		nhqm = [i for i in nhqm if "=" in i]
+		nhqm = {measure.split("=")[0]: measure.split("=")[1] for measure in nhqm}
+		if ("MT" not in nhqm):
+			self.error('[{}.{}.{}] no vram metric'.format(rig_name, name, device_id), errors)
+		else:
+			vram_temp = int(nhqm["MT"])-128	
+			if(vram_temp>max_vram_temp):
+				self.error('[{}.{}.{}] current vram temp: {} exceed max vram temp {}.'.format(rig_name, name, device_id, vram_temp, max_vram_temp), errors)
 		if(power>max_power):
 			self.error('[{}.{}.{}] current power usage: {} exceed max power {}.'.format(rig_name, name, device_id, power, max_power), errors)
-		if(temp>max_tem):
-			self.error('[{}.{}.{}] current temp: {} exceed max temp {}.'.format(rig_name, name, device_id, temp, max_tem), errors)
+		if(temp_encoded<0):
+			self.error('[{}.{}.{}] negative current core temp: {}.'.format(rig_name, name, device_id, temp_encoded), errors)
+		elif(core_temp>max_core_temp):
+			self.error('[{}.{}.{}] current core temp: {} exceed max core temp {}.'.format(rig_name, name, device_id, core_temp, max_core_temp), errors)
 		if(hr<min_hr):
 			self.error('[{}.{}.{}] current hash rate: {} lower than min hash rate {}.'.format(rig_name, name, device_id, hr, min_hr), errors)
 		if(fan_speed<min_fan_speed):
-			self.error('[{}.{}] current fan speed: {} lower than min fan speed {}.'.format(rig_name, name, fan_speed, min_fan_speed), errors)
+			self.error('[{}.{}.{}] current fan speed: {} lower than min fan speed {}.'.format(rig_name, name, device_id, fan_speed, min_fan_speed), errors)
 
 	def error(self, message, errors, send_email=False):
 		self.logger.error(message)
--- Test/monitor_test.py
+++ Test/monitor_test.py
@@ -12,8 +12,11 @@ class MonitorTest(unittest.TestCase):
         '[id_accepted_speed_0] speedAccepted = 0.',
         '[id_rejected_ratio_high] rejected_ratio = 50.0%.',
         'Script error [id_invalid_profile.Invalid_card] device not recognized',
-        '[id_low_hash_rate_id_2.GeForce RTX 3060.2] current temp: 65535.0 exceed max temp 68.0.',
-        '[id_low_hash_rate_id_2.GeForce RTX 3060.2] current hash rate: 25.22 lower than min hash rate 43.0.'
+        '[id_low_hash_rate_id_2.GeForce RTX 3060.2] current hash rate: 25.22 lower than min hash rate 43.0.',
+        '[id_low_hash_rate_id_2.GeForce RTX 3060.2] negative current core temp: -1.0.',
+        '[id_low_hash_rate_id_2.GeForce RTX 3060.2] no vram metric',
+        '[id_power_high.GeForce RTX 3090.0] current power usage: 400.0 exceed max power 350.0.',
+        '[id_fan_speed_low.GeForce RTX 3090.0] current fan speed: 4.0 lower than min fan speed 10.0.'
     ])
     
     def setUp(self):
