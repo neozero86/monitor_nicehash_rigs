0f43201f1487606599713918911e78b3357d9a9f
Matias Egea <matiasegea@gmail.com>
2022-08-06 11:31:25 -0300

    indentation fix

--- Main/main.py
+++ Main/main.py
@@ -7,16 +7,17 @@ from Main.mail_sender import MailSender
 from Main.report.reporter import Reporter
 
 def main():
-	conf, rigs, devices = Configuration.constants()
-	logger = Logger()
-	api = NicehashPrivateApi(conf.organisation_id, conf.key, conf.secret)
-	api_adapter = ApiAdapter(api)
-	email_sender = MailSender(conf.mail.gmail_username,
+    conf, rigs, devices = Configuration.constants()
+    logger = Logger()
+    api = NicehashPrivateApi(conf.organisation_id, conf.key, conf.secret)
+    api_adapter = ApiAdapter(api)
+    email_sender = MailSender(conf.mail.gmail_username,
                                     conf.mail.gmail_password,
                                     conf.mail.notification_email,
                                     conf.mail.email_subject)
-	Reporter.instance().init(email_sender, {"interval_seconds":86400, "production": True}, logger)
-	monitor = Monitor(logger, api_adapter, email_sender, rigs, devices, conf.error_threshold, conf.max_rejected_ratio, conf.polling_interval_sec)
-	monitor.run()
+    Reporter.instance().init(email_sender, {"interval_seconds":86400, "production": True}, logger)
+    monitor = Monitor(logger, api_adapter, email_sender, rigs, devices,
+    conf.error_threshold, conf.max_rejected_ratio, conf.polling_interval_sec)
+    monitor.run()
 if __name__ == '__main__':
     main()
