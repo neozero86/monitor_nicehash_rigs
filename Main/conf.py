12ef48bfc07f8a239d640be712b98dba84815125
Matias Egea <matiasegea@gmail.com>
2022-08-10 18:22:55 -0300

    adapting to netlify

--- Main/conf.py
+++ Main/conf.py
@@ -1,8 +1,13 @@
 import json
+import os
 from collections import namedtuple
 
 class Configuration():
     def constants(filename = 'Main/conf.json'):
+        if "CONF_FILE" in os.environ:
+            filename='Main/conf_temp.json'
+            with open(filename, 'w') as file:
+                file.write(os.getenv('CONF_FILE'))
         with open(filename) as data_file:
             conf = json.load(data_file, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
             rigs = {r.name: r for r in conf.rigs}
